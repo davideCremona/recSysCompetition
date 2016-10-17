4cc018086abe8a824a16047e2b0dd5d5c5f5d59e
davide_cremona <davide_cremona@10.169.151.230>
2016-10-17 17:01:39 +0200

    data exploration #1

--- source/test.py
+++ source/test.py
@@ -2,7 +2,22 @@ import numpy as np
 import pandas as pd
 
 
-itemProfileData = pd.read_csv('../data/item_profile.csv', sep='\t')
+"""
+Variabili costanti (path dei file, ...)
+"""
+data_path = '../data/'
+filename_interactions = 'interactions.csv'
+filename_item_profile = 'item_profile.csv'
+filename_target_users = 'target_users.csv'
+filename_user_profile = 'user_profile.csv'
 
-print itemProfileData.head()
+"""
+Lettura dei file csv
+"""
+raw_item_profile = pd.read_csv(data_path+filename_item_profile, sep='\t')
+raw_interactions = pd.read_csv(data_path+filename_interactions, sep='\t')
+raw_target_users = pd.read_csv(data_path+filename_target_users, sep='\t')
+raw_user_profile = pd.read_csv(data_path+filename_user_profile, sep='\t')
+
+print raw_user_profile.head()
 
