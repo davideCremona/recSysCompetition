40f09305083fc109b8c693aabeee5938dc6de7f5
davide_cremona <davide_cremona@10.169.151.230>
2016-10-20 16:13:49 +0200

    top rated recommendation

--- source/topofthepops.py
+++ source/topofthepops.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import random
 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 """
 Variabili costanti (path dei file, ...)
 """
@@ -27,17 +29,60 @@ raw_user_profile = pd.read_csv(data_path+filename_user_profile, sep='\t')
 
 unique_items_ids = np.unique(raw_interactions[interactions_item_id])
 
-filtered_items = []
+active_items = raw_item_profile[raw_item_profile['active_during_test']==1]
+filtered_items_ids    = active_items['id']
+filtered_interactions = raw_interactions[raw_interactions['item_id'].isin(filtered_items_ids)]
 
-for item in unique_items_ids:
-    if raw_item_profile[raw_item_profile['id']==item]['active_during_test'].tolist()[0] == 1:
-        filtered_items.append(item)
+maxTimestamp = filtered_interactions['created_at'].max()
+minTimestamp = filtered_interactions['created_at'].min()
 
-print len(filtered_items), len(unique_items_ids)
+print "MaxTimestamp:",maxTimestamp,"minTimestamp:",minTimestamp
 
-sorted_items = np.empty([len(filtered_items),2], dtype="string")
+
+filtered_interactions['created_at'] = filtered_interactions['created_at'].apply(lambda x: np.interp(x,[minTimestamp,maxTimestamp],[0,1]))
+filtered_interactions['scored_interaction'] = filtered_interactions['interaction_type']*filtered_interactions['created_at']
+
+def getScore(item_id):
+    interactions_for_item = filtered_interactions[filtered_interactions['item_id']==item_id]
+    return interactions_for_item['scored_interaction'].sum()
+
+active_items['score'] = active_items['id'].apply(lambda x: getScore(item_id=x))
+
+to_recommend = active_items.sort(['score'], ascending=0).head()['id'].tolist()
 data         = np.empty([len(raw_target_users[0:]['user_id']), 2], dtype="string")
 
+for uid in raw_target_users[0:]['user_id']:
+    recommended_items = str(to_recommend[0])+" "+str(to_recommend[1])+" "+str(to_recommend[2])+" "+str(to_recommend[3])+" "+str(to_recommend[4])
+    row = [str(uid), recommended_items]
+    data = np.vstack([data, row])
+
+np.savetxt(data_path+'submission_02.csv', data, delimiter=',', fmt="%s")
+
+
+
+
+
+
+"""
+for item_id in filtered_items_ids:
+    interactions_for_item = filtered_interactions[filtered_interactions['item_id']==item_id]
+    interactions_for_item['created_at'] = interactions_for_item['created_at'].apply(lambda x: np.interp(x,[minTimestamp,maxTimestamp],[0,1]))
+    interactions_for_item['scored_interaction'] = interactions_for_item['interaction_type']*interactions_for_item['created_at']
+    active_items['score'] = interactions_for_item['scored_interaction'].sum()
+    scores.append(item_score)
+    print "Score for item: "+str(item_id)+" is "+str(item_score)
+
+
+scored_ids = np.column_stack((filtered_items_ids.tolist(),scores))
+scored_ids = scored_ids.sort(key=lambda x:x[1])
+
+print scored_ids[0:5]
+
+"""
+"""
+sorted_items = np.empty([len(filtered_items),2], dtype="string")
+data         = np.empty([len(raw_target_users[0:]['user_id']), 2], dtype="string")
+"""
 #for item in filtered_items:
 #    score = sum(raw_interactions[raw_interactions['item_id']==item]['interaction_type'].tolist())
 #    print score
